10b5546a0282de4ed18d16b1c127cbcc4482f301
Dave <dave@rouser.org>
2013-07-10 20:13:41 -0400

    added bower to test libs

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,9 +3,13 @@
 (function () {
     describe('Give it some context', function () {
         describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-
+            it('should equal itself', function () {
+                expect(1).to.be.equal(1);
             });
+
+            it('should not equal 2', function() {
+                expect(1).not.to.be.equal(2);
+            })
         });
     });
 })();
